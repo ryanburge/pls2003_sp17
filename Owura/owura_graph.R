ea1c74e5030f8dbda1817dfe10521a8796fc5e60
Ryan Burge <ryanburge@gmail.com>
2017-04-19 08:13:10 -0500

    forgot some stuff.

--- Owura/owura_graph.R
+++ Owura/owura_graph.R
@@ -1,9 +1,14 @@
 library(reshape2)
 library(ggplot2)
 
-owura_personnel <- read_csv("D:/pls2003_sp17/owura_personnel.csv")
+owura_personnel <- read_csv("D://pls2003_sp17/Owura/owura_personnel.csv")
 owura <- melt(owura_personnel, id.vars = c("Country Name"))
 owura <- owura[-(1:24), , drop = FALSE]
+
+owura$value <- as.numeric(owura$value)
+owura$country <- owura$`Country Name`
+
+
 
 ggplot(owura, aes(x=variable, y= value/1000, label = country, color = country, label = country)) + 
   geom_line(aes(group = country), size=1) +
